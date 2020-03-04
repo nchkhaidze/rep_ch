0702673f39fffc3968f1285cb609299ae715db7e
nchkhaidze <nchkhaidze@users.noreply.github.com>
2020-03-04 09:38:01 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -95,16 +95,16 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     neighbor_count++;
                 }
             }
-            int average[3] = {0, 0, 0};
+            float average[3] = {0, 0, 0};
             for (int i = 0; i < neighbor_count; i++)
             {
-                average[0] = average[0] + neighbors[i].rgbtRed;
-                average[1] = average[1] + neighbors[i].rgbtGreen;
-                average[2] = average[2] + neighbors[i].rgbtBlue;
+                average[0] = round(average[0] + (float)neighbors[i].rgbtRed);
+                average[1] = round(average[1] + (float)neighbors[i].rgbtRed);
+                average[2] = round(average[2] + (float)neighbors[i].rgbtRed);
             }
-            image[h][w].rgbtRed = average[0] / neighbor_count;
-            image[h][w].rgbtGreen = average[1] / neighbor_count;
-            image[h][w].rgbtBlue = average[2] / neighbor_count;
+            image[h][w].rgbtRed = round(average[0] / (float)neighbor_count);
+            image[h][w].rgbtGreen = round(average[1] / (float)neighbor_count);
+            image[h][w].rgbtBlue = round(average[2] / (float)neighbor_count);
         }
     }
     free(*original);
