ba474767dbf2a0ba8fbbbd59f2722d65dd1b863f
nchkhaidze <nchkhaidze@users.noreply.github.com>
2020-03-05 18:03:55 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -129,7 +129,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 {
                     if (!in_bounds(h + hn, w + wn, height, width))
                     {
-                        kernel_count++;
+                        printf("out of bounds\n");
                         break;
                     }
                     Gx[0] = Gx[0] + (original[h + hn][w + wn].rgbtRed * Gx_kernel[kernel_count]);
